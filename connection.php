63ed634f0a1a9e238b0aab6dfe351d06876e7456
Marius <mulamamarius@gmail.com>
2021-04-12 23:43:15 +0300

    Added the cart page and added credentials to connection file

--- connection.php
+++ connection.php
@@ -1,16 +1,12 @@
 <?php
-//Initialize system variables
-$host_server =$user = $passcode = $database ='';
-//Import system variables
-include "authentication/credentials.php";
 
-$servername = $host_server;
-$username = $user;
-$password = $passcode;
-$dbname = $database;
+$host_server = "localhost";
+$user = "root";
+$password = "";
+$database = "merch_store";
 
 //create and execute connection based off credentials
-$conn = new mysqli($servername,$username,$password,$dbname);
+$conn = new mysqli($host_server,$user,$password,$database);
 
 // the connection
 if ($conn->connect_error) {
